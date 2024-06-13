e79ab83a66753709e374c09d4d24f94a4daf97ea
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-13 03:34:16 -0300

    feat: hash user password

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UserPresenter } from './user.presenter';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) { }
 
   @Post()
-  create(@Body() data: CreateUserDTO) {
-    return this.usersService.create(data);
+  async create(@Body() data: CreateUserDTO) {
+    const user = await this.usersService.create(data);
+
+    return new UserPresenter(user);
   }
 }
--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserRoles } from './dto/user-roles';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -11,8 +12,13 @@ export class UsersService {
     return this.prismaService.user.create({
       data: {
         ...data,
+        password: this.generateHash(data.password),
         roles: [UserRoles.PARTNER],
       },
     });
+  }
+
+  generateHash(password: string) {
+    return bcrypt.hashSync(password, 10);
   }
 }
